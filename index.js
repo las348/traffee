40534edc86b547add8515d5d28bc0c0385e7129e
Laureni Wilkinson <las348@nyu.edu>
2020-05-19 13:46:32 -0400

    updated index

--- index.js
+++ index.js
@@ -4,107 +4,89 @@ const axios = require("axios");
 const gitHubAPI = require('github-api');
 const generateMarkdown = require("./generateMarkdown");
 
-writeToFile();
+const questions = inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "github",
+            message: "Please write your GitHub username: ",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Please write your name: ",
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "Please input your project title: "
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please describe your project: "
+        },
+        {
+            type: "input",
+            name: "technologies",
+            message: "Please input the technologies used: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please input the installation instructions: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please describe the usage for project: ",
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "Please list any licenses: ",
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Please list any contributers: ",
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Please input your tests: ",
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "Please input your questions: ",
+        }
+    ])
 
+function writeToFile(fileName, data) {
+    //user Input
+    let userData = generateMarkdown(data)
 
-function writeToFile(data) {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "github",
-                message: "Please write your GitHub username: ",
-            },
-            {
-                type: "input",
-                name: "name",
-                message: "Please write your name: ",
-            },
-            {
-                type: "input",
-                name: "title",
-                message: "Please input your project title: "
-            },
-            {
-                type: "input",
-                name: "description",
-                message: "Please describe your project: "
-            },
-            {
-                type: "input",
-                name: "technologies",
-                message: "Please input the technologies used: "
-            },
-            {
-                type: "input",
-                name: "installation",
-                message: "Please input the installation instructions: ",
-            },
-            {
-                type: "input",
-                name: "usage",
-                message: "Please describe the usage for project: ",
-            },
-            {
-                type: "input",
-                name: "license",
-                message: "Please list any licenses: ",
-            },
-            {
-                type: "input",
-                name: "contributing",
-                message: "Please list any contributers: ",
-            },
-            {
-                type: "input",
-                name: "tests",
-                message: "Please input your tests: ",
-            },
-            {
-                type: "input",
-                name: "questions",
-                message: "Please input your questions: ",
-            }
-        ])
+    // fs.writeFile(file, data[, options], callback)
+    fs.writeFile("README.md", userData, function (error, data) {
+        console.log("README done");
 
-        //github API
-        .then(async function getUser() {
-            try {
-                const { githubAPI } = await axios.get({
-                    profilePic: owner.avatar.url,
-                    profileURL: owner.url
-                });
-                const { URL } = await axios.get(
-                    `https://api.github.com/users/${githubAPI}`
-                );
-                console.log(URL);
+    })
+};
 
-                //user Input
-                let userData = generateMarkdown(data)
+function init() {
+    let queryGithubURL = "https://api.github.com/users/" + questions.github;
 
-                // fs.writeFile(file, data[, options], callback)
-                fs.writeFile("README.md", userData, function (error, data) {
-                    console.log("README done");
+    axios.get(queryGithubURL).then(
+        function (gitProfile) {
+            let data = githubProfile.data;
+            // Add github profile picture
+            let profilePic = data.avatar.url;
+            let profileURL = data.url;
 
-                })
-            }
-            catch (err) {
-                console.log(err);
-            }
-        });
-} // end of writeToFile function
+            // userChoices.githubPic = data.avatar_url;
+            // userChoices.url = data.html_url;
+        })
+};
 
-
-// console log errors ex 200, 404
-
-
-// const questions = [
-// ];
-// function writeToFile(fileName, data) {
-// }
-// function init() {
-// }
-// init();
-
-// ask for the info with inquirer and then call the github api and then you need to build a string with  
-// regular markdown  readme and the variables coming from your questions + you GitHub response
+init();
